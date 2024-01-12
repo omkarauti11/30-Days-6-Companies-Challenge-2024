ee23bab6a7962b6f88f6a8eb8d911c35f4994ba4
Omkar Auti <122270586+omkarauti11@users.noreply.github.com>
2024-01-13 03:20:54 +0530

    Add files via upload

---
+++ Day13/825_Friends_Of_Appropriate_Ages.cpp
@@ -0,0 +1,39 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// solution
+
+class Solution
+{
+public:
+    bool request(int a, int b)
+    {
+        return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100));
+    }
+    int numFriendRequests(vector<int> &ages)
+    {
+        unordered_map<int, int> count;
+
+        for (int &age : ages)
+        {
+            count[age]++;
+        }
+
+        int res = 0;
+
+        for (auto &a : count)
+        {
+            for (auto &b : count)
+            {
+                if (request(a.first, b.first))
+                {
+                    res += a.second * (b.second - (a.first == b.first ? 1 : 0));
+                }
+            }
+        }
+        
+        return res;
+    }
+};
